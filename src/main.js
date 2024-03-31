6685f9b40cb8bc453bffb43b7b32bf0eabcefeb2
laurent bernabé <laurent.bernabe@gmail.com>
2024-04-01 01:43:43 +0200

    add empty chess board

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import ChessBoard from '@loloof64/chessboard-component/dist';
 
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,14 @@ import legacy from '@vitejs/plugin-legacy'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          // treat all tags with a dash as custom elements
+          isCustomElement: (tag) => tag.includes('-')
+        }
+      }
+    }),
     legacy({
       targets: ['>0.01%', 'defaults'],
     }),
