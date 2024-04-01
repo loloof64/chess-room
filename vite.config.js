e3039328ff70a63416fb5e7a7f379f63f8df6e17
laurent bernabé <laurent.bernabe@gmail.com>
2024-04-01 16:23:40 +0200

    attempt to fix website with GH action

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,13 @@ export default defineConfig({
       targets: ['>0.01%', 'defaults'],
     }),
   ],
+  build: {
+    modulePreload: {
+      resolveDependencies: (filename, deps, { hostId, hostType }) => {
+        return deps.map((dep) => {
+          return `/docs/${dep}`
+        });
+      }
+    }
+  }
 })
