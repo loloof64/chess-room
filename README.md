517a386f51894771930fc724b14fa5a031920087
laurent bernabé <laurent.bernabe@gmail.com>
2024-04-05 02:01:39 +0200

    add history component on game page

--- README.md
+++ README.md
@@ -22,10 +22,18 @@ export {appId, databaseId, collectionId};
 
 ## Credits
 
+### Fonts
+
+Font freeSerif was downloaded from [FontSpace](https://www.fontspace.com/freeserif-font-f13277).
+
 ### Images
 
 Some images have been downloaded from [SvgRepo](https://www.svgrepo.com).
 
-* Icon has been downloaded from https://www.svgrepo.com/svg/85723/chess
-* copy has been downloaded from https://www.svgrepo.com/svg/525288/
-* paste has been downloaded from https://www.svgrepo.com/svg/525279/clipboard-text
+* Icon : https://www.svgrepo.com/svg/85723/chess
+* copy : https://www.svgrepo.com/svg/525288/
+* paste : https://www.svgrepo.com/svg/525279/clipboard-text
+* backward : https://www.svgrepo.com/svg/436646/backward-fill
+* backward_end : https://www.svgrepo.com/svg/436645/backward-end-fill
+* forward : https://www.svgrepo.com/svg/436766/forward-fill
+* forward_end : https://www.svgrepo.com/svg/436765/forward-end-fill
--- src/pages/GamePage.vue
+++ src/pages/GamePage.vue
@@ -1,10 +1,14 @@
 <script setup>
 import { ref, onMounted, onBeforeUnmount } from 'vue';
+import History from '@/components/History.vue';
 let boardSize = ref('300');
+
+const board = ref();
+const history = ref();
 
 let resizeBoard = () => {
     const minSize = window.innerWidth < window.innerHeight ? window.innerWidth : window.innerHeight;
-    boardSize.value = minSize * 0.99;
+    boardSize.value = minSize * 0.82;
 };
 
 onMounted(() => {
@@ -18,7 +22,26 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-    <loloof64-chessboard ref="board" :size="boardSize" />
+    <div id="root">
+        <div id="gameZone">
+            <history ref="history" />
+            <loloof64-chessboard ref="board" :size="boardSize" />
+        </div>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#root {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+#gameZone {
+ display: flex;
+ flex-direction: column;
+ justify-content: start;
+ align-items: center;
+}
+</style>
